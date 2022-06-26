afd3b1acedafa0bcdc0126f8905737a4100de95c
Pratik Dhungana <pratik.tan10@gmail.com>
2022-06-25 22:40:10 -0500

    Add files via upload

---
+++ JS-Slider/script.js
@@ -0,0 +1,36 @@
+window.addEventListener("load", function(){
+    const slideCount = document.querySelectorAll("#slider-wrapper ul li").length;
+    const slideWidth = document.querySelectorAll("#slider-wrapper").offsetWidth;
+    const totalWidth = slideCount * slideWidth + "px";
+
+    const slider = document.querySelector("#slider-wrapper ul");
+    const next = document.querySelector("#next");
+    const prev = document.querySelector("#prev");
+
+    let leftPosition = 0;
+    let counter = 0;
+    slider.style.width = totalWidth;
+
+    next.addEventListener("click", function(evt){
+        evt.preventDefault();
+        counter++;
+        if(counter==slideCount){
+            counter = 0;
+        }
+        leftPosition = `-${counter*slideWidth}px`;
+        slider.style.left = leftPosition;
+                
+    });
+
+    prev.addEventListener("click", function(evt){
+        evt.preventDefault();
+        counter--;
+        if(counter<0){
+            counter = slideCount -1;
+        }
+        leftPosition = `-${counter*slideWidth}px`;
+        slider.style.left = leftPosition;
+                
+    });
+
+});
